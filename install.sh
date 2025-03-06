8393921eacc7f2a51d02e547c1d4d34115511d0d
vinhphuctadang <vinhphuctadang@gmail.com>
2025-03-07 00:06:54 +0700

    fix: script to download fluxd from release

--- install.sh
+++ install.sh
@@ -4,17 +4,20 @@
 LIB_PATH=/usr/local/lib
 BIN_PATH=/usr/local/bin
 ARCH=$(uname -m)
-# install evmone
+# evmone
 if [ ! -f "${LIB_PATH}/libevmone.dylib" ] ; then
     sudo cp ./chain/libs/evmone/$ARCH/* $LIB_PATH
 fi
 
-# install golana
+# golana
 if [ ! -f "${LIB_PATH}/libgolana.dylib" ] ; then 
     sudo cp ./chain/libs/golana/$ARCH/* $LIB_PATH
 fi
 
 # fluxd
 if [ ! -f "${BIN_PATH}/fluxd" ] ; then 
+    mkdir -p ./chain/binary
+    curl -L "https://github.com/FluxAstromeshLabs/astromesh-demo/releases/download/v0.1/fluxd.${ARCH}" -o ./chain/binary/fluxd
+    chmod +x ./chain/binary/fluxd
     sudo cp ./chain/binary/$ARCH/fluxd $BIN_PATH
 fi
