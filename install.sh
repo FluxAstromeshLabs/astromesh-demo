a1b5bdf8e2fd7e88cfb2b93d1dd882b4359f4f05
vinhphuctadang <vinhphuctadang@gmail.com>
2025-03-07 16:01:21 +0700

    feat: install libwasmvm

--- install.sh
+++ install.sh
@@ -6,12 +6,20 @@ BIN_PATH=/usr/local/bin
 ARCH=$(uname -m)
 # evmone
 if [ ! -f "${LIB_PATH}/libevmone.dylib" ] ; then
+    echo "installing evmone (evm)"
     sudo cp ./chain/libs/evmone/$ARCH/* $LIB_PATH
 fi
 
 # golana
 if [ ! -f "${LIB_PATH}/libgolana.dylib" ] ; then 
+    echo "installing golana (svm)"
     sudo cp ./chain/libs/golana/$ARCH/* $LIB_PATH
+fi
+
+# wasmvm
+if [ ! -f "${LIB_PATH}/libwasmvm.dylib" ] ; then
+    echo "installing lib wasmvm" 
+    sudo curl -L "https://github.com/CosmWasm/wasmvm/releases/download/v1.5.2/libwasmvm.dylib" -o /usr/local/lib/libwasmvm.dylib
 fi
 
 # fluxd
