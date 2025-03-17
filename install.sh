28992ee4c6fc6b40d271afabd9e0b7e51951c64d
vinhphuctadang <vinhphuctadang@gmail.com>
2025-03-17 22:53:06 +0700

    chore: revamp install script

--- install.sh
+++ install.sh
@@ -5,27 +5,20 @@ LIB_PATH=/usr/local/lib
 BIN_PATH=/usr/local/bin
 ARCH=$(uname -m)
 # evmone
-if [ ! -f "${LIB_PATH}/libevmone.dylib" ] ; then
-    echo "installing evmone (evm)"
-    sudo cp ./chain/libs/evmone/$ARCH/* $LIB_PATH
-fi
+echo "installing evmone (evm)"
+sudo cp ./chain/libs/evmone/$ARCH/* $LIB_PATH
 
 # golana
-if [ ! -f "${LIB_PATH}/libgolana.dylib" ] ; then 
-    echo "installing golana (svm)"
-    sudo cp ./chain/libs/golana/$ARCH/* $LIB_PATH
-fi
+echo "installing golana (svm)"
+sudo cp ./chain/libs/golana/$ARCH/* $LIB_PATH
 
 # wasmvm
-if [ ! -f "${LIB_PATH}/libwasmvm.dylib" ] ; then
-    echo "installing lib wasmvm" 
-    sudo curl -L "https://github.com/CosmWasm/wasmvm/releases/download/v1.5.2/libwasmvm.dylib" -o /usr/local/lib/libwasmvm.dylib
-fi
+echo "installing lib wasmvm" 
+sudo curl -L "https://github.com/CosmWasm/wasmvm/releases/download/v1.5.2/libwasmvm.dylib" -o /usr/local/lib/libwasmvm.dylib
 
 # fluxd
-if [ ! -f "${BIN_PATH}/fluxd" ] ; then 
-    mkdir -p ./chain/binary
-    curl -L "https://github.com/FluxAstromeshLabs/astromesh-demo/releases/download/v0.1/fluxd.${ARCH}" -o ./chain/binary/fluxd
-    chmod +x ./chain/binary/fluxd
-    sudo cp ./chain/binary/fluxd $BIN_PATH
-fi
+echo "install fluxd"
+mkdir -p ./chain/binary
+curl -L "https://github.com/FluxAstromeshLabs/astromesh-demo/releases/download/v0.1/fluxd.${ARCH}" -o ./chain/binary/fluxd
+chmod +x ./chain/binary/fluxd
+sudo cp ./chain/binary/fluxd $BIN_PATH
--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-./install.sh
-
 rm -rf ~/.fluxd
 cp -r ./chain/.fluxd ~/.fluxd
 DYLD_LIBRARY_PATH=/usr/local/lib ./chain/binary/fluxd start
