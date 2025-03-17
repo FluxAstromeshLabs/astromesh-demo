369ec727d15429cfe8c450b5d18c99c482fc9d1b
vinhphuctadang <vinhphuctadang@gmail.com>
2025-03-17 23:24:16 +0700

    fix: docker pull always to have latest images

--- start-docker.sh
+++ start-docker.sh
@@ -7,4 +7,4 @@ docker rm -f $(docker ps -aq)
 
 echo "running fluxd container"
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/i1x2i1m1
-docker run  --pull=missing -d --volume=$HOME/.fluxd:/root/.fluxd -p 26657:26657 -p 26656:26656 -p 10337:10337 -p 9900:9900 --name fluxd public.ecr.aws/i1x2i1m1/fluxd:dev
+docker run  --pull=always -d --volume=$HOME/.fluxd:/root/.fluxd -p 26657:26657 -p 26656:26656 -p 10337:10337 -p 9900:9900 --name fluxd public.ecr.aws/i1x2i1m1/fluxd:dev
